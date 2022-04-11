d5cf8ea7cd2c70ecc95cefa912570fb1e3fc4f07
Erich Wagner <erich.wagner@tu-dortmund.de>
2022-04-11 22:34:09 +0200

    v504 keine Heizspannung => fertig

--- v504/plot.py
+++ v504/plot.py
@@ -1,6 +1,13 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+from scipy.optimize import curve_fit                        # Funktionsfit:     popt, pcov = curve_fit(func, xdata, ydata) 
+from uncertainties import ufloat                            # Fehler:           fehlerwert =  ulfaot(x, err)
+from uncertainties.unumpy import uarray                     # Array von Fehler: fehlerarray =  uarray(array, errarray)
+from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
+                                  std_devs as stds)         # Abweichung:       stds(fehlerarray) = errarray
+from uncertainties import unumpy as unp 
+import scipy.constants as const
 
 md1 = pd.read_csv('tables/md1.csv')
 np.savetxt('tables/md1.txt', md1.values, fmt='%.3f')
@@ -53,19 +60,18 @@ plt.clf()
 # plot 5:
 plt.plot(U, k5, 'xr', label = "Kennlinie 5", zorder=2, alpha = 0.7)
 
-z = np.polyfit(U, k5, 3)
-p = np.poly1d(z)
-xp = np.linspace(-50, 280, 100)
-plt.plot(xp, p(xp), '-b', label = "Ausgleichsrechnung", linewidth = 1, zorder=1)
+def lsr(u, a, b):
+    return (4/9) * const.epsilon_0 * np.sqrt(2 * const.elementary_charge/ const.electron_mass) * u**(b)/a**2
 
-dp = p.deriv()
-ddp = dp.deriv()
-dddp = ddp.deriv()
+para, pcov = curve_fit(lsr, U[:15], k5[:15])
+a, b = para
+fa, fb = np.sqrt(np.diag(pcov))
 
-x = 0.0001046/8.479e-07
+ua = ufloat(a, fa) 
+ub = ufloat(b, fb)
 
-xp = np.linspace(-50, 280, 100)
-plt.plot(x, p(x), 'Db', label = "Wendepunkt", markersize = 6, zorder=3)
+xx = np.linspace(0, 280, 100)
+plt.plot(xx, lsr(xx, noms(ua), noms(ub)), '-b', label = "Ausgleichsfunktion", linewidth = 1, zorder=1)
 
 plt.xlabel(r'$U \, / \, \mathrm{V}$')
 plt.ylabel(r'$I \, / \, \mathrm{mA}$')
@@ -76,3 +82,30 @@ plt.ylim(-0.1, 2.3)
 
 plt.savefig('build/plot_k5.pdf', bbox_inches = "tight")
 plt.clf() 
+
+# plot 6: Aufgabe d
+md2 = pd.read_csv('tables/md2.csv')
+np.savetxt('tables/md2.txt', md2.values, fmt='%.4f')
+U, I = np.genfromtxt('tables/md2.txt', unpack=True)
+
+plt.plot(U, I, 'xr', label = "Messdaten",  alpha = 0.7)
+
+def f(u, a, b):
+    return a * np.exp(-u / b)
+
+para, pcov = curve_fit(f, U, I)
+a, b = para
+fa, fb = np.sqrt(np.diag(pcov))
+
+xx = np.linspace(-0.5, 1.5, 100)
+plt.plot(xx, f(xx, a, b), '-b', label = "Ausgleichsfunktion", linewidth = 1)
+
+plt.xlabel(r'$U \, / \, \mathrm{V}$')
+plt.ylabel(r'$I \, / \, \mathrm{nA}$')
+plt.legend(loc="best")     
+plt.grid(True)
+plt.xlim(0, 1.05)
+plt.ylim(-0.5, 9)
+
+plt.savefig('build/plot6.pdf', bbox_inches = "tight")
+plt.clf()
--- vXXX/plot.py
+++ vXXX/plot.py
@@ -1,5 +1,13 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit                        # Funktionsfit:     popt, pcov = curve_fit(func, xdata, ydata) 
+from uncertainties import ufloat                            # Fehler:           fehlerwert =  ulfaot(x, err)
+from uncertainties.unumpy import uarray                     # Array von Fehler: fehlerarray =  uarray(array, errarray)
+from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
+                                  std_devs as stds)         # Abweichung:       stds(fehlerarray) = errarray
+from uncertainties import unumpy as unp 
+import scipy.constants as const
 
 x = np.linspace(0, 10, 1000)
 y = x ** np.sin(x)
